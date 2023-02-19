cf45435dc8aae1fa2e7e64e71eed29ca92580058
denisnazarov97 <ricki19967@yandex.ru>
2023-02-19 16:27:49 +0300

    Initial commit

---
+++ src/main/java/com/denisnazarov/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.denisnazarov.spring.rest.service;
+
+import com.denisnazarov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
